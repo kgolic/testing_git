f546636c6c3f3aa94ec7fc5d7e6b653ad1fac291
kgolic <kristin.golic@gmail.com>
2014-02-02 08:54:59 -0600

    added motorcycle class

---
+++ motorcycle.rb
@@ -0,0 +1,17 @@
+class Motorcycle
+  def initialize(make, color)
+    # Instance variables
+    @make = make
+    @color = color
+  end
+
+  def startEngine
+    if (@engineState)
+      'Engine is already Running'
+    else
+     @engineState = true
+      'Engine Idle'
+    end
+  end
+end
+
